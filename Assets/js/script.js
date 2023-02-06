dc81b0f6d6981e24ea61d7bfd5896e63f3b19b5f
kamalrajtz <112368016+dev-kamalraj@users.noreply.github.com>
2023-02-06 18:47:24 +0530

    Updated Forex site.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,3 +13,15 @@ fetch("./footer.html")
     .then(data => {
         document.querySelector("footer").innerHTML = data;
     });
+
+$(function () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar").removeClass("navbar-bg");
+            $(".navbar").addClass("navbar-bg2").style.top = "0";
+        } else {
+            $(".navbar").removeClass("navbar-bg2");
+            $(".navbar").addClass("navbar-bg").style.top = "-50px";
+        }
+    });
+});
